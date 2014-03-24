c2ada5aeb1b3ef3e6f511d0d8afaa1a847c88db7
Daniel J. Jones <djjones@tampabay.rr.com>
2014-03-23 21:53:17 -0400

    Ver 2.0

--- Reciever/Reciever.ino
+++ Reciever/Reciever.ino
@@ -1,5 +1,5 @@
 #include <VirtualWire.h>
-
+//#include <DigiUSB.h>
 #define redPin       0 // this is the pin the red pin is on
 #define greenPin     1 // this is the pin the green pin is on 
 #define bluePin      4 // this is the pin the blue pin is on 
@@ -11,55 +11,50 @@ void setup()
   vw_set_rx_pin(receive_pin);
   vw_setup(2000); // Bits per sec
   vw_rx_start();  // Start the receiver PLL running
+ // DigiUSB.begin();
   
 
- 
+
 }
 
 void loop()
 {
   uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
-  
+  int val  ; 
+
   if (vw_get_message(buf, &buflen)) // Non-blocking
+  {
+    int val;     //this is the stage of colors we are on 
+   // DigiUSB.println(val);
+    switch(val)    // 1st stage no color
     {
-     int val  ;                       //this is the stage of colors we are on 
-     if(val == 0);                    // 1st stage no color
-     {
-     analogWrite(redPin, 255);
-     analogWrite(bluePin, 255);
-     analogWrite(greenPin, 255);
-     val++;                           // 1 is added to the value to make it go to the next color the next time
-     }
-     
-     
-     if(val == 1);                    // 2nd stage red color
-     {
-     analogWrite(redPin, 0);
-     analogWrite(bluePin, 255);
-     analogWrite(greenPin, 255);
-     val++;                           // 1 is added to the value to make it go to the next color the next time     
-     }
-     
-     
-     if(val == 2);                    // 3rd stage blue color
-     {
-     analogWrite(redPin, 255);
-     analogWrite(bluePin, 0);
-     analogWrite(greenPin, 255);
-     val++;                           // 1 is added to the value to make it go to the next color the next time
-     }
-          
-     
-     if(val == 3);                    // 4th stage green color
-     {
-     analogWrite(redPin, 255);
-     analogWrite(bluePin, 255);
-     analogWrite(greenPin, 0);
-     val = 0;                         // value resets so the loop starts over
-     }
-               
+     case 0:
+      analogWrite(redPin, 255);
+      analogWrite(bluePin, 255);
+      analogWrite(greenPin, 255);
+                                 // 1 is added to the value to make it go to the next color the next time
+                      // 2nd stage red color
+     case 1:
+      analogWrite(redPin, 0);
+      analogWrite(bluePin, 255);
+      analogWrite(greenPin, 255);
+                                 // 1 is added to the value to make it go to the next color the next time     
+      
+    case 2:                    // 3rd stage blue color
+      analogWrite(redPin, 255);
+      analogWrite(bluePin, 0);
+      analogWrite(greenPin, 255);
+          // 1 is added to the value to make it go to the next color the next time
+  
+    case 3:                   // 4th stage green color
+      analogWrite(redPin, 255);
+      analogWrite(bluePin, 255);
+      analogWrite(greenPin, 0);
+                             // value resets so the loop starts over
     }
+  //DigiUSB.delay(10);
+  }
 }
 
 
@@ -75,18 +70,19 @@ void loop()
 
 
 /*  int mail = vw_have_message();        //mail = ture when there is a message waiting
-  
-  if (mail == true);               //you've got mail!
-  {
-   int failsafe = vw_wait_rx_max(50);  //waits for message for 50ms if nothing returns false
-   
-   if (failsafe == true);              //and the mail you got is real mail!
-   {
-     vw_get_message(buf, &buflen);
-   }
-  
  
-  }
+ if (mail == true);               //you've got mail!
+ {
+ int failsafe = vw_wait_rx_max(50);  //waits for message for 50ms if nothing returns false
+ 
+ if (failsafe == true);              //and the mail you got is real mail!
+ {
+ vw_get_message(buf, &buflen);
+ }
+ 
+ 
+ }
+ 
+ */
 
-*/
 
--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,27 +1,37 @@
 #include <VirtualWire.h>
 
-#define button_pin      1 //this is the pin that the button is pluged into
-#define transmit_pin    2 //this is the pin the transmitter is pluged into
-#define indicator       13//this is the onboard led to tell when a message has been sent
+#define button_pin      4 //this is the pin that the button is pluged into
+#define transmit_pin    10 //this is the pin the transmitter is pluged into
+#define indicator       11//this is the onboard led to tell when a message has been sent
 
 void setup()
 {
+  int i;
   pinMode(indicator, OUTPUT);  //Sets up the onboard led to be an indicator
   pinMode(button_pin, INPUT);   //Sets up the button for input
   vw_set_tx_pin(transmit_pin); //Sets up the output pin for VW
-  vw_setup(2000);              //starts vitualwire
+  vw_setup(2000);    //starts vitualwire
 }
 
 void loop()
 {
   int buttonState = digitalRead(button_pin);
-  
+
   if (buttonState == HIGH)
   {
-    vw_send((uint8_t *)1,1);
-    vw_wait_tx();
+    int i;
+    if (i < 3)
+    {
+      vw_send((uint8_t *)i,1);
+      vw_wait_tx();
+      analogWrite(indicator, HIGH);
+      delay(500);
+      analogWrite(indicator, LOW);
+      i++;
+    }
+     i = 0;
   }
-
+  
 
 
 
